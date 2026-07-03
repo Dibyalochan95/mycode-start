487d499f6dd397b7ae9342b6158c856301797b85
Dibyalochan95 <lipu29061995@gmail.com>
2026-07-03 08:52:55 +0530

    calculated amount after validation

--- CI.py
+++ CI.py
@@ -2,12 +2,12 @@ principal = float(input("Enter the principal amount: "))
 rate = float(input("Enter rate of interest: "))
 time = int(input("Enter time in years: "))
 n = int(input("Enter no times principal compounded: "))
-amount = principal*(1 + rate/(100*n))**(n*time)
 if principal <= 0 or rate <= 0 or time <= 0 or n <= 0:
     print("*"*30)
     print("Invalid input. Please try again ")
     print("*"*30)
 else:
+    amount = principal*(1 + rate/(100*n))**(n*time)
     print("*"*30)
     print("Amount payable :", round(amount,2))
     print("*"*30)
@@ -17,8 +17,8 @@ else:
 breakdown = input("Enter yes or no :")
 if breakdown == "yes":
     for month in range(1,time*12 +1):
-        amount = principal*(1 + rate/(100*12))**(month)
-        print("Month :", month, ":", round(amount))
+        amount = principal*(1 + rate/(100*n))**(month)
+        print("Month :", month, ":", round(amount,2))
 elif breakdown == "no":
     print("Have a nice day")
 else:
